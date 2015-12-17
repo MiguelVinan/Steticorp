a82e15407007bfaa79cf75a8d0b75da2ae4ce88f
luis <luisio231990@hotmail.com>
2015-12-17 08:12:37 -0500

    barnner terminado en Home

---
+++ layouts/barnner.php
@@ -0,0 +1,13 @@
+<section class="barnner  ">
+  <div class="barnnerContainer col-xs-12 col-sm-8 col-md-6">
+    <div class="barnnerCircle">
+        <span>-20%</span>
+    </div>
+
+    <p class="barnnerText">
+      Traemos el 20% descuento en la semana del 22 al 27 de Junio. En todos nuestros servicios.
+    </p>
+
+  </div>
+
+</section>
