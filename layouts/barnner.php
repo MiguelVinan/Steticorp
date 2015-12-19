25ef9fbac3eb2eea301f8376549a99e00c8e8882
Miguel Alejandro <mvinan@icloud.com>
2015-12-18 19:05:07 -0500

    was added container class

--- layouts/barnner.php
+++ layouts/barnner.php
@@ -1,13 +1,15 @@
-<section class="barnner  ">
-  <div class="barnnerContainer col-xs-12 col-sm-8 col-md-6">
-    <div class="barnnerCircle">
-        <span>-20%</span>
+<section class="barnner">
+  <div class="container">
+    <div class="barnnerContainer col-xs-12 col-sm-8 col-md-6">
+      <div class="barnnerCircle">
+          <span>-20%</span>
+      </div>
+    
+      <p class="barnnerText">
+        Traemos el 20% descuento en la semana del 22 al 27 de Junio. En todos nuestros servicios.
+      </p>
+    
     </div>
-
-    <p class="barnnerText">
-      Traemos el 20% descuento en la semana del 22 al 27 de Junio. En todos nuestros servicios.
-    </p>
-
   </div>
 
 </section>
