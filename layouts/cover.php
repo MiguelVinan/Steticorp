dc6571532c18093b36a2e59466492d138129101b
Miguel Alejandro <mvinan@icloud.com>
2015-12-19 23:59:09 -0500

    styles for articles in blog

--- layouts/cover.php
+++ layouts/cover.php
@@ -1,4 +1,10 @@
-<div class="cover u-before"  style="background-image:url('<?= get_template_directory_uri() . "/assets/images/wellness-skinCare.jpg" ?>');">
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+  <?php
+    $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post -> ID ), 'large' );
+    $url = $thumb['0'];
+  ?>
+
+<section class="cover u-before"  style="background-image:url( <?= $url ?> );" >
   <article class="container">
     <img src="<?= get_template_directory_uri() . '/assets/images/florLoto.svg' ?>" alt="">
     <h1 class="specialTitle col-xs-12 col-md-6"> <?= the_title(); ?></h1>
@@ -8,10 +14,8 @@
   </article>
 
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
 <?php endwhile; else : ?>
   <p><?php _e( 'Lo sentimos, no tenemos imagen por el momento :(' ); ?></p>
 <?php endif; ?>
 
-</div>
+</section>
--- page-24.php
+++ page-24.php
@@ -2,8 +2,29 @@
 
   <?php include(locate_template('layouts/cover.php')) ?>
   <?php include(locate_template('layouts/search-box.php')) ?>
-  <section class="container">
+  <section class="container u-paddingSection" style="height:800px;">
     
+    <?php rewind_posts(); ?>
+    <?php query_posts(array ('paged' => $paged,'posts_per_page' => 9, 'post_type'=> 'articulos' )) ?>
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <?php
+        $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post -> ID ), 'medium' );
+        $url = $thumb['0'];
+      ?>
+
+    <article class="article col-xs-12 col-md-4">
+      <time><?= the_date('j \d\e F, Y') ?></time>
+      <a href="<?php the_permalink(); ?>">
+        <div class="article-image" style="background-image:url( <?= $url ?> );">
+          <h3><?php the_title(); ?></h3>
+        </div>
+      </a>
+    </article>
+
+    <?php endwhile; else : ?>
+      <p><?php _e( 'Lo sentimos, no tenemos imagen por el momento :(' ); ?></p>
+    <?php endif; ?>
+
   </section>
   
 <?php get_footer(); ?>
