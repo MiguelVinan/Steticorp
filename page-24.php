f0885ac6da8008706afc31f1632d00cbfe6d04e0
Miguel Alejandro <mvinan@icloud.com>
2015-12-22 16:41:45 -0500

    was added two custom post types and convert articles in component

--- page-24.php
+++ page-24.php
@@ -12,14 +12,7 @@
         $url = $thumb['0'];
       ?>
 
-    <article class="article col-xs-12 col-sm-6 col-md-4">
-      <time><?php the_time('j \d\e F, Y') ?></time>
-      <a href="<?php the_permalink(); ?>">
-        <div class="article-image" style="background-image:url( <?= $url ?> );">
-          <h3><?php the_title(); ?></h3>
-        </div>
-      </a>
-    </article>
+    <?php include(locate_template('layouts/article.php')) ?>
 
     <?php endwhile; ?>
     <div class="pagination-links col-xs-12 text-center">
