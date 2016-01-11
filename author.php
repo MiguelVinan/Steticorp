57a94c2cf9230edf009fb822a46c4a753b384e86
luis <luisio231990@hotmail.com>
2016-01-11 12:24:49 -0500

    problemaSolucionadoTxtoAuthor

--- author.php
+++ author.php
@@ -6,33 +6,45 @@
     $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
     ?>
 
-  <section class="authorBibliografia container u-paddingSection">
-    <div class="authorData row">
-      <div class="authorFoto col-xs-10 col-sm-10" style="background-image:url('<?= get_template_directory_uri() . "/assets/images/Silvana.jpg" ?>');" >
-        <div class="authorSilvana" >
+  <section class="author container u-paddingSection">
+    <div class="authorContainer row">
+
+
+      <div class="authorInfo">
+        <div class="authorData col-xs-12 col-sm-10 col-sm-pull-2 col-md-8">
+          <div class="authorFoto" style="background-image:url('<?= get_template_directory_uri() . "/assets/images/Silvana.jpg" ?>');" >
+          </div>
+        </div>
+
+        <div class="authorParrafo col-xs-12 col-sm-8 col-sm-push-4 col-md-8">
+          <div class="authorParrafo-Container">
+            <div class="authorBibliografia">
+              <p><?= $curauth->description; ?>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <p><?= $curauth->description; ?>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div class="authorBtn">
+              <div class="authorClik">
+                <a href="#" class="btn-more">Visita nuestro blog</a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="authorParrafo col-xs-12 col-sm-9 col-md-9">
-      <div class="authorDerecha">
-        <p><?= $curauth->description; ?></p>
-        <p class="hidden-xs hidden-sm">
-        </p>
-      </div>
-      <div class="authorBtn col-xs-12 col-sm-12">
-        <div class="authorClik">
-          <a href="#" class="btn-more">Visita nuestro blog</a>
+
+      <div class="authorVisita">
+        <div class="authorTitle">
+          <h2>Te invito a que visites nuestro blog </h2>
         </div>
+        <figure class="authorDireccion">
+          <img src="<?= get_template_directory_uri() . "/assets/images/arrow-line.svg" ?>" alt="" />
+        </figure>
       </div>
     </div>
   </section>
-  <section class="authorVisita container u-paddingSection">
-    <div class="authorDireccion hidden-xs hidden-sm" style="background-image:url('<?= get_template_directory_uri() . "/assets/images/arrow-line.svg" ?>');" >
-    </div>
-    <div class="authorTitle text-center col-xs-12">
-      <h2>Te invito a que visites nuestro blog</h2>
-    </div>
-  </section>
+
+
+
+
 
 <?php include(locate_template('layouts/suscripcion.php')) ?>
 <?php get_footer(); ?>
--- layouts/cover-author.php
+++ layouts/cover-author.php
@@ -6,7 +6,7 @@
     <div class="authorParrafo cover-content col-xs-12 col-md-6">
       <p>Nuestro deber es ayudar a quienes quieren mejorar su imagen personal para que puedan moldear su figura y así nuestro cliente se sienta más seguro de sí mismo, lo que nos diferencia es que lo hacemos con tratamientos no invasivos y eficaces. Hacemos esto nsulta totalmente gratuita.</p>
     </div>
-    <div class="authorSociales hidden-xs hidden-sm col-md-12">
+    <div class="authorSociales hidden-xs hidden-sm col-md-12 col-md-push-0 ">
       <?php include(locate_template('layouts/redesSociales-Silvana.php')) ?>
     </div>
   </article>
