bb46b4ae54498a86d85f9f4de5dacb82e099b1cb
Miguel Alejandro <mvinan@icloud.com>
2016-01-12 08:03:02 -0500

    Adding urls to clients layout

--- layouts/client.php
+++ layouts/client.php
@@ -3,7 +3,7 @@
     <div class="clientesContent">
       <div class="clientesFoto" style="background-image:url('<?= the_sub_field('client_imagen') ?>');">
       </div>
-      <h4><?php the_sub_field('client_nickname') ?></h4>
+      <h4><a href="<?= the_sub_field('client_url') ?> "><?php the_sub_field('client_nickname') ?></a></h4>
     </div>
     <p class=""><?php the_sub_field('client_testimonial') ?></p>
   </div>
