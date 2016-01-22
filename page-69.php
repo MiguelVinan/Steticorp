d8ffb0879376aa09c3f53da53a6b7b10dbf5fb76
luisantos <hidal@Mac-mini.local>
2016-01-22 00:21:38 -0500

    cambios-Nosotros-Suscripcion

--- page-69.php
+++ page-69.php
@@ -4,20 +4,20 @@
   <section class="u-paddingSection container">
 
     <?php rewind_posts(); ?>
-    <?php query_posts(array ('posts_per_page' => 0, 'post_type'=> 'corporales' )) ?>
+    <?php query_posts(array ('posts_per_page' => 9999, 'post_type'=> 'corporales' )) ?>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
       <?php
         $thumb = wp_get_attachment_image_src( get_post_thumbnail_id( $post -> ID ), 'medium' );
         $url = $thumb['0'];
       ?>
-      
+
       <?php include(locate_template('layouts/single-service.php')) ?>
-      
-    
+
+
     <?php endwhile; else : ?>
       <p><?php _e( 'Lo sentimos, aún no hemos colocado un tratamiento' ); ?></p>
     <?php endif; ?>
-    
+
   </section>
 
 <?php get_footer(); ?>
